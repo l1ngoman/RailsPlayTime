a5502309f7d39c63a9ab25348bd846b0e314c4b8
learn <learn@learn08.localdomain>
2018-10-29 17:21:07 -0700

    game is working

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -5,8 +5,22 @@ class GameController < ApplicationController
 
   def try
     @guess = params[:guess]
-
-    if :guess 
+    if @guess == nil
+      game()
+    end
+    if @guess != nil
+      compare()
+    end
     render 'try.html.erb'
+  end
+
+  def compare
+    if @guess > cookies[:secret]
+      @feedback = "TOO HIGH"
+    elsif @guess < cookies[:secret]
+      @feedback = "TOO LOW"
+    elsif @guess == cookies[:secret]
+      @feedback = "YOU GOT IT, DUDE!!!!👍 👍 👍"
+    end
   end
 end
